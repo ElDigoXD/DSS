539a06e14f9da7286a934756853e8673e8249531
Diego Sanz <digosanf@gmail.com>
2023-11-05 13:30:57 +0100

    Creado el módulo vista, el archivo era muy grande

---
+++ dss/views/__init__.py
@@ -0,0 +1,116 @@
+import csv
+import json
+from datetime import date, datetime, time
+from typing import Any
+
+from django.http import HttpRequest, HttpResponse, JsonResponse
+from django.shortcuts import render
+from requests import Response, get
+
+from dss.models import Consumo, ConsumoDev, Precio_kw, Precio_venta, Vecino
+
+from .info_vecino import info_vecino
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    vecinos = Vecino.objects.all()
+    return render(request, "index.html", {"vecinos": vecinos})
+
+
+def load_vecinos(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("")
+    result = ConsumoDev.objects.filter(
+        fecha__gte=datetime.date(2010, 10, 1), fecha__lt=datetime.date(2010, 11, 1)
+    )
+    print(result)
+    for r in result:
+        Consumo.objects.create(
+            vecino=Vecino.objects.get(nombre="pepe"),
+            kw_media_consumidos=r.kw_media_consumidos,
+            fecha=datetime.date(2023, 1, r.fecha.day),
+            hora=r.hora,
+            duracion_m=60,
+        )
+    return HttpResponse(
+        f"<html><body>It is now {datetime.datetime.now()}.</body></html>"
+    )
+
+
+def load_precios(request: HttpRequest) -> HttpResponse:
+    res: Response = get("https://api.preciodelaluz.org/v1/prices/all?zone=PCB")
+    print(res.text)
+    a: dict[str, dict[str, Any]] = json.loads(res.text)
+
+    a2 = Precio_kw.objects.filter(
+        fecha=datetime.strptime(a["00-01"]["date"], "%d-%m-%Y")
+    )
+    if len(a2) == 0:
+        print("vacío")
+        for h, datos in a.items():
+            new = Precio_kw.objects.create(
+                precio=datos["price"] / 1000,
+                fecha=datetime.strptime(datos["date"], "%d-%m-%Y"),
+                hora=datetime.strptime(datos["hour"][:2], "%H"),
+                duracion_m=60,
+            )
+            new.save()
+    else:
+        print(a2)
+
+    # for h, datos in a.items():
+    #     print(datos["price"])
+
+    return HttpResponse(f"<html> {a} <html>")
+
+
+def load_data(request: HttpRequest) -> HttpResponse:
+
+    res = get("https://api.preciodelaluz.org/v1/prices/all?zone=PCB")
+    print(res.text)
+
+    # stext = [x for ]
+    return HttpResponse(f"<html> {res.text} <html>")
+    with open("datos consumo.csv") as csvfile:
+        reader = csv.reader(csvfile, delimiter=";")
+        for i, (date, time, consumo) in enumerate(reader):
+            if i == 0:
+                continue
+            d = datetime.strptime(date, "%d/%m/%Y")
+            t = datetime.strptime(time, "%H:%M:%S")
+            # print(d, t, float(consumo.replace(",", ".")))
+            new = ConsumoDev.objects.create(
+                kw_media_consumidos=float(consumo.replace(",", ".")),
+                fecha=d,
+                hora=t,
+                duracion_m=60,
+            )
+
+    return HttpResponse(html)
+
+
+def test(request: HttpRequest) -> HttpResponse:
+    res = get("https://apidatos.ree.es/es/datos/mercados/precios-mercados-tiempo-real?start_date=2023-01-01T00:00&end_date=2023-01-31T23:59&time_trunc=hour")
+
+    values = json.loads(res.text)["included"][0]["attributes"]["values"]
+    for value in values:
+        new = Precio_venta.objects.get_or_create(
+            fecha=datetime.fromisoformat(value["datetime"]),
+            hora=datetime.fromisoformat(value["datetime"]),
+            defaults={
+                "precio": value["value"]/1000,
+                "duracion_m": 60,
+            }
+        )
+        print(new)
+
+    print(values)
+    return HttpResponse()
+
+    # new = Precio_kw.objects.create(
+    #     precio=2,
+    #     fecha=datetime.datetime.fromisoformat(),
+    #     hora=,
+    #     duracion_m=60,
+    # )
+
+    return JsonResponse(json.loads(res.text))
